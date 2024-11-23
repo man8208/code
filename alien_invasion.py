e955c46faeec36b8b5f5677f43597ac9086ea2bb
man8208 <man8208@163.com>
2024-11-23 15:58:52 +0800

    Add files via upload

---
+++ alien_invasion.py
@@ -0,0 +1,37 @@
+import sys
+
+import pygame
+
+from settings import Settings
+
+
+class AlienInvasion:
+    """管理游戏资源和行为的类。"""
+
+    def __init__(self):
+        """初始化游戏并创建游戏资源。"""
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("太空飞船")
+
+    def run_game(self):
+        """开始游戏的主循环。"""
+        while True:
+            # 监视键盘和鼠标事件。
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # 每次循环都重绘屏幕。
+            self.screen.fill(self.settings.bg_color)
+
+            # 让最近绘制的屏蔽可见。
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
